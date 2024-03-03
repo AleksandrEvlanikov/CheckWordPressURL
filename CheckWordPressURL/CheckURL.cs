700362c0db1550895f897d4f17c7a93db5c649a2
Aleksandr <aleksandr.evlanicov@gmail.com>
2024-03-03 14:36:50 +0300

    Добавьте файлы проекта.

---
+++ CheckWordPressURL/CheckURL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CheckWordPressURL
+{
+    internal class CheckURL
+    {
+        internal bool CheckURLOnWordPress(string url)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+
+                    string htmlPage = client.GetStringAsync(url).Result;
+
+                    if (htmlPage.Contains("<meta name=generator content=WordPress"))
+                    {
+                        return true;
+                    }
+                    if (htmlPage.Contains("wp-content") || htmlPage.Contains("wp-includes") || htmlPage.Contains("wp-admin"))
+                    {
+                        return true;
+                    }
+                    return false;
+                } 
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine($"Ошибка авторизации {e}");
+                return false;
+            }
+        }
+    }
+}
---
+++ CheckWordPressURL/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CheckWordPressURL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckURL CheckURL = new CheckURL();
+            Console.WriteLine("Введите URL ");
+            string urlUserInput = Console.ReadLine();
+
+            if(CheckURL.CheckURLOnWordPress(urlUserInput))
+            {
+                Console.WriteLine("Сайт реализован с помощью WordPress.");
+            }
+            else
+            {
+                Console.WriteLine("Сайт не реализован с помощью WordPress.");
+            }
+        }
+    }
+}
